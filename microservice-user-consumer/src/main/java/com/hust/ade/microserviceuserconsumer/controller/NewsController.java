30061a732b35e3bb3cc13b1031b7e71a715eb493
toujours33 <1012242313@qq.com>
2019-05-06 10:43:49 +0800

    自定义微服务元数据信息

--- microservice-user-consumer/src/main/java/com/hust/ade/microserviceuserconsumer/controller/NewsController.java
+++ microservice-user-consumer/src/main/java/com/hust/ade/microserviceuserconsumer/controller/NewsController.java
@@ -2,10 +2,14 @@ package com.hust.ade.microserviceuserconsumer.controller;
 
 import com.hust.ade.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class NewsController {
@@ -13,9 +17,17 @@ public class NewsController {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	@GetMapping("/news/user/{id}")
 	public User viewUser(@PathVariable Long id){
 		return restTemplate.getForObject("http://localhost:8088/user/" + id, User.class);
+	}
+
+	@GetMapping("/user-instance")
+	public List<ServiceInstance> showInfo(){
+		return this.discoveryClient.getInstances("microservice-user-provider");
 	}
 
 }
